0d780a05b44c9bd7053d385ae18332ad94183e78
Helli1995 <35336195+Helli1995@users.noreply.github.com>
2023-08-21 23:39:54 +0200

    Add files via upload

--- src/rtpghi~.c
+++ src/rtpghi~.c
@@ -1,4 +1,4 @@
-//
+ //
 //  test.c
 //  
 //
@@ -20,47 +20,62 @@ typedef struct _rtpghi_tilde {
     t_float do_causal_pd;
     t_symbol window_type;
     phaseret_rtpghi_state_s* sta_pd;
-    //float _Complex* c;     ///
-    //float* s;               ///both added because PD crashed when using rtpghi_execute
+    ltfat_complex_s  *c;     ///
+    float *s;
+    
     t_float f;
     
-    //t_inlet *x_in2;
-    
-    t_outlet*x_out;
-    t_outlet*x_out_1;
+    //t_inlet *x_in1;
+    t_outlet *x_out;
+    t_outlet *x_out1;
     
 } t_rtpghi_tilde;
-
-/*void init_plan(t_init_plan *x)
-{
-
-
-    
-}*/
 
 
 t_int *rtpghi_tilde_perform(t_int *w)
 {
-  t_rtpghi_tilde *x = (t_rtpghi_tilde *)(w[1]);
-  t_sample    *in1 =      (t_sample *)(w[2]);
+  //t_sample    *in1 =      (t_sample *)(w[2]);
+  float *s =      (float *)(w[1]);
+  ltfat_complex_s *c = (ltfat_complex_s *)w[2];
+  //t_sample    *in2 =      (t_sample *)(w[2]);
   t_sample    *out =      (t_sample *)(w[3]);
-  t_sample    *out_1 =      (t_sample *)(w[4]);
+  t_sample    *out1 =      (t_sample *)(w[4]);
   int            n =             (int)(w[5]);
+  t_rtpghi_tilde *x = (t_rtpghi_tilde *)(w[6]);
     
+    t_sample r = 0;
+    t_sample i = 0;
+  
     
-    //phaseret_rtpghi_execute_s(x->sta_pd, x->s, x->c);
 
-    *out = *in1;
-    *out_1 = *in1;
+    phaseret_rtpghi_execute_s(x->sta_pd, s, c);
     
-
-  return (w+6);
+    //t_sample *real = NULL;
+    //t_sample *imag = NULL;
+    
+    
+    while (n--) {
+        //ltfat_complex_s *c_samp = *c_++;
+        r = (t_sample) crealf(*c++);
+        i =  (t_sample) cimagf(*c++);
+        
+        //(x->c) = *(x->c)++;
+        //t_sample real = (t_sample) cimag(*(x->c)++);
+        //t_sample imag = (t_sample) creal(*(x->c)++);
+                    
+        *out++ = r;
+        *out1++ = i;
+    }
+  return (w+7);
 }
 
 void rtpghi_tilde_dsp(t_rtpghi_tilde *x, t_signal **sp)
 {
-  dsp_add(rtpghi_tilde_perform, 5, x,
-          sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[0]->s_n);
+    
+    
+    x-> c = ltfat_malloc_sc(sp[0]->s_n);
+  dsp_add(rtpghi_tilde_perform, 6, sp[0]->s_vec, x->c , sp[1]->s_vec,
+          sp[2]->s_vec, sp[0]->s_n, x);
 }
 
 
@@ -77,11 +92,9 @@ void *rtpghi_tilde_new(t_symbol *s, int argc, t_atom *argv)
     x->do_causal_pd=atom_getfloat(argv+4);
     x->window_type= *atom_getsymbol(argv+5);
     x->sta_pd=NULL;
-    
-    
-    //int N = x->M_pd;
-    //x->c=ltfat_calloc_sc(N);
-    //x->s=ltfat_calloc_s(N);
+    x-> c = NULL;
+    x-> s = NULL;
+  
     
     ltfat_int M = x->M_pd;
     post("stft_length [M]: %d\n", M);
@@ -99,32 +112,29 @@ void *rtpghi_tilde_new(t_symbol *s, int argc, t_atom *argv)
     double tol = x->tol_pd;
     post("Tolerance [tol]: %f\n", tol);
     
-    //int do_causal = x->do_causal_pd;
     int do_causal = 0;
     post("Causal yes=1, no=0, [do_causal]: %d\n", do_causal);
     
     phaseret_rtpghi_init_s(gamma, W, a , M, tol, do_causal, &(x->sta_pd));
     
     post("initialised rtpghi plan at adress: %p\n", &(x->sta_pd));
-    //outlet_new(&x->x_obj, &s_pointer);
-    //outlet_new(&x->x_obj, &s_symbol);
+
+    //x->x_in1=inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
+    x->x_out = outlet_new(&x->x_obj, gensym("signal"));
+    x->x_out1 = outlet_new(&x->x_obj, gensym("signal"));
     
-    x->x_out = outlet_new(&x->x_obj, &s_signal);
-    x->x_out_1 = outlet_new(&x->x_obj, &s_signal);
-  
   return (void *)x;
 }
 
 
 void rtpghi_tilde_free(t_rtpghi_tilde *x)
 {
-    //phaseret_rtpghi_state_d* sta = x->sta_pd;
     post("destroyed state at adrr: %p\n", &(x->sta_pd));
     phaseret_rtpghi_done_s(&(x->sta_pd));
-    //ltfat_free(x->c);
-    //ltfat_free(x->s);
+    ltfat_free(x->c);
+    //inlet_free(x->x_in1);
     outlet_free(x->x_out);
-    outlet_free(x->x_out_1);
+    outlet_free(x->x_out1);
     
 }
 
